e3a237fb29e16793d8a40634dc21646acd069342
vlad.alexeev@gmail.com <vlad.alexeev@gmail.com@51ad4c1b-0a73-1a0a-7529-31e15faef216>
2012-05-29 12:03:50 +0000

    Start developing new version 1.0.1 Dummy action for Batch replace on folder.

---
+++ trunk/eclipse-web-kit/src/com/eclipse/web/kit/popup/actions/BatchReplaceAction.java
@@ -0,0 +1,12 @@
+package com.eclipse.web.kit.popup.actions;
+
+import org.eclipse.jface.action.IAction;
+
+public class BatchReplaceAction extends FolderPopupAction {
+
+	@Override
+	public void run(IAction action) {
+		System.out.println("BatchReplaceAction");
+	}
+
+}
