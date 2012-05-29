dbad99bb99c0880149c8ac4b6ddcc48330b73b6c
vlad.alexeev@gmail.com <vlad.alexeev@gmail.com@51ad4c1b-0a73-1a0a-7529-31e15faef216>
2012-05-29 13:29:35 +0000

    BatchReplaceDialog

--- eclipse-web-kit/src/com/eclipse/web/kit/popup/actions/BatchReplaceAction.java
+++ eclipse-web-kit/src/com/eclipse/web/kit/popup/actions/BatchReplaceAction.java
@@ -2,11 +2,20 @@ package com.eclipse.web.kit.popup.actions;
 
 import org.eclipse.jface.action.IAction;
 
+import com.eclipse.web.kit.util.SwtUtil;
+import com.eclipse.web.kit.views.BatchReplaceDialog;
+
 public class BatchReplaceAction extends FolderPopupAction {
 
 	@Override
 	public void run(IAction action) {
 		System.out.println("BatchReplaceAction");
+		BatchReplaceDialog dialog=new BatchReplaceDialog(SwtUtil.getActiveWorkbenchWindow().getShell());
+		if (dialog.open()) {
+			System.out.println("Execute batch replace");
+		} else {
+			System.out.println("Canceled");
+		}
 	}
 
 }
