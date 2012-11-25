5d09191248daae21514538e80a3ed866c2af05a6
vlad.alexeev <vlad.alexeev@51ad4c1b-0a73-1a0a-7529-31e15faef216>
2012-11-25 11:55:16 +0000

    Project property page for news feeds descriptions selection.

---
+++ eclipse-web-kit/src/com/eclipse/web/kit/preferences/NewsFeedsPreferences.java
@@ -0,0 +1,43 @@
+package com.eclipse.web.kit.preferences;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.swt.widgets.Shell;
+
+import com.eclipse.web.kit.Activator;
+import com.eclipse.web.kit.overlay.FieldEditorPropertyPage;
+import com.eclipse.web.kit.preferences.editors.AddActionHandler;
+import com.eclipse.web.kit.preferences.editors.StringListFieldEditor;
+import com.eclipse.web.kit.util.FileUtil;
+import com.eclipse.web.kit.util.SwtUtil;
+
+public class NewsFeedsPreferences extends FieldEditorPropertyPage {
+
+	public NewsFeedsPreferences() {
+		super(GRID);
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+	}
+	
+	@Override
+	protected String getPageId() {
+		return PreferenceConstants.PAGE_ID_NEWS_FEEDS;
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		StringListFieldEditor descFieldEditor=
+				new StringListFieldEditor(PreferenceConstants.P_NEWS_FEEDS_DESCRIPTIONS, "News feeds descriptions", getFieldEditorParent());
+		descFieldEditor.setAddActionHandler(new AddActionHandler() {
+			@Override
+			public String doAdd() {
+				String fileName=FileUtil.selectAnyFile(new Shell());
+				IProject project=SwtUtil.getActiveProject();
+				System.out.println("Selecte file name = "+fileName);
+				System.out.println("project path = "+project.getLocation().toString());
+				return FileUtil.createRelativePath2(project.getLocation().toString(), fileName);
+			}
+		});
+		
+		addField(descFieldEditor);
+	}
+
+}
---
+++ eclipse-web-kit/src/com/eclipse/web/kit/preferences/editors/AddActionHandler.java
@@ -0,0 +1,10 @@
+package com.eclipse.web.kit.preferences.editors;
+
+/**
+ * ActionHandler for StringListFieldEditor for add action.
+ * @author Vlad
+ *
+ */
+public interface AddActionHandler {
+	public String doAdd();
+}
