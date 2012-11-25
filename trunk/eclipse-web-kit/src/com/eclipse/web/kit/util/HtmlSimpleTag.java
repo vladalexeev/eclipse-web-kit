5c7ff36da860f4c739018bd0b814ca071600b848
vlad.alexeev <vlad.alexeev@51ad4c1b-0a73-1a0a-7529-31e15faef216>
2012-11-25 09:50:37 +0000

    HtmlParser

---
+++ trunk/eclipse-web-kit/src/com/eclipse/web/kit/util/HtmlSimpleTag.java
@@ -0,0 +1,28 @@
+package com.eclipse.web.kit.util;
+
+import java.util.Map;
+
+public class HtmlSimpleTag {
+	private String tagName;
+	private Map<String,String> attributes;
+	
+	public HtmlSimpleTag() {
+		
+	}
+
+	public String getTagName() {
+		return tagName;
+	}
+
+	public void setTagName(String tagName) {
+		this.tagName = tagName;
+	}
+
+	public Map<String, String> getAttributes() {
+		return attributes;
+	}
+
+	public void setAttributes(Map<String, String> attributes) {
+		this.attributes = attributes;
+	}
+}
